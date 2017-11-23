1c10de3a5a6d0d93eb7ec05b629a9e8226d4bc3a
Nicolaks <32768134+Nicolaks@users.noreply.github.com>
2017-11-23 18:07:45 +0100

    Ajout de la javadoc
    pour lancer la javadoc il faut créer un répertoire doc dans le dossier projetpoo master,
    et ensuite mettre la commande suivante. j'ai pas encore réussi à mettre en UTF-8.
    javadoc -d "doc" projetpoo/*.java -cp "projetpoo/scheduleio.jar"

--- projetpoo/Activity.java
+++ projetpoo/Activity.java
@@ -1,25 +1,50 @@
 package projetpoo;
-
+/**
+ * La class Activity
+ */
 import java.util.GregorianCalendar;
 
-public class Activity {//class Activity permettant représenter des crénaux horaires
+/**
+   * Lance la class Activity qui permet de représenter des crénaux horaires
+   */
+
+public class Activity {
 
   private String descr;//Description du crénaux
   private int duree;//En minute
 
+  /**
+   * Le constructeur prend en entrée une description et une durée.
+   * @param descr
+   * La description doit être un String.
+   * @param duree
+   * La durée doit être un int.
+   */
   public Activity (String descr, int duree) {
     this.descr=descr;
     this.duree=duree;
   }
 
+ /**
+  * @return
+  * Retourne la représentation
+  */
   public String getRepresentation () {
     return descr+" "+duree;
   }
 
+  /**
+   * @return
+   *Retourne la description
+   */
   public String getAct () {
     return this.descr;
   }
 
+  /**
+   * @return
+   * Retourne la durée
+   */
   public int getDuree () {
     //System.out.println("duree test"+this.duree);
     return this.duree;
