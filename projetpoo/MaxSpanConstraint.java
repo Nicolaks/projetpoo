e6eb6332321571e386674c4ee5d53e097ab7695b
Nicolaks <32768134+Nicolaks@users.noreply.github.com>
2017-11-24 14:08:26 +0100

    Nicolas
    Mise en place de la javadoc

--- projetpoo/MaxSpanConstraint.java
+++ projetpoo/MaxSpanConstraint.java
@@ -1,5 +1,7 @@
 package projetpoo;
-
+/**
+   * Lance la class MaxSpanConstraint qui implémente l'interface Constraint.
+   */
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.time.LocalDateTime;
@@ -8,19 +10,33 @@ import java.util.Set;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
-
+/**
+   * La class MaxSpanConstraint.
+   */
 public class MaxSpanConstraint implements Constraint {
 
 
   ArrayList<Activity> listActivite = new ArrayList<> ();
   int dureeTotal;
 
-
+  /**
+     *
+     *@param listActivite
+     * Prend comme premier paramètre un ArrayList de type Activity.
+     * @param dureeTotal
+     * Prend en second paramètre un int qui représente la durée totale que l'on ne veut pas dépasser.
+     */
   public MaxSpanConstraint (ArrayList<Activity> listActivite,int dureeTotal) {
     this.listActivite = listActivite;
     this.dureeTotal = dureeTotal;
   }
 
+  /**
+     * @param edt
+     * Prend en entrée un Schedule et vérifie si il satisfait les constraintes.
+     * @return
+     * Retourne un booléen.
+     */
   @Override
   public boolean isSatisfied(Schedule edt) {
     int dureeTouteAct = 0;
