7ab9f179c24866476bfa0b3338af1208e6e2cddf
Nicolaks <32768134+Nicolaks@users.noreply.github.com>
2017-11-24 13:52:34 +0100

    Nicolas
    Mise en place de la javadoc

--- projetpoo/BinaryConstraint.java
+++ projetpoo/BinaryConstraint.java
@@ -1,5 +1,7 @@
 package projetpoo;
-
+/**
+ * La class BinaryConstraint
+ */
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.time.LocalDateTime;
@@ -9,26 +11,55 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+   * Lance la class BinaryConstraint qui gère les contraintes si une activité est avant une autre.
+   * Elle implémente l'interface Constraint.
+   */
+
 public abstract class BinaryConstraint implements Constraint {
 
   protected Activity first;
   protected Activity second;
-
+  /**
+     * @param first
+     * le paramètre first est de type Activity.
+     * @param second
+     * Le paramètre second est aussi de type Activity.
+     */
   public BinaryConstraint (Activity first,Activity second) {
     this.first=first;
     this.second=second;
   }
-
+  /**
+     * @return
+     * Retourne la première durée.
+     */
   public int getDureeFirst () {
     return first.getDuree();
   }
-
+  /**
+     * @return
+     * Retourne la durée de la seconde activité
+     */
   public int getDureeSecond () {
     return second.getDuree();
   }
 
-  public abstract boolean isSatisfied(GregorianCalendar date1,GregorianCalendar date2);
+  /**
+     * Classe isSatisfied abstraite qui vérifie si les activités sont compatibles.
+     * @param date1
+     * Prend en premier paramètre un GregorianCalendar.
+     * @param date2
+     * Prend en second paramètre un autre GregorianCalendar.
+     */
 
+  public abstract boolean isSatisfied(GregorianCalendar date1,GregorianCalendar date2);
+  /**
+     * @return
+     * Retourne un booléen.
+     * @param edt
+     * Prend en paramètre un Schedule.
+     */
   public boolean isSatisfied(Schedule edt){
     GregorianCalendar date1 = edt.getDateForActivity(this.first);
     GregorianCalendar date2 = edt.getDateForActivity(this.second);
